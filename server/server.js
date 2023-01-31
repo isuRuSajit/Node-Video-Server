d76900ae02aba78bf4774898b3c38c8bf8036962
isuRuSajit <89340506+isuRuSajit@users.noreply.github.com>
2023-01-31 23:18:29 +0530

    server create

---
+++ server/server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
+dotenv.config();
+app.use(bodyParser.urlencoded({ extended: false }));
+// Extracts json data and makes it easy readable to us
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Running ");
+});
+
+app.use("/api/signUp", require("./routes/signUp"));
+
+mongoose.set("strictQuery", true);
+mongoose
+  .connect(process.env.ATLAS_URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("Database Successfully Connected"))
+  .catch((error) => console.log(error));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server Running on Port - ${port}`);
+});
+module.exports = app;
